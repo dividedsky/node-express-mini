e9a37bcbfeac808414d7f94e49ab6c7ed292c8f3
dividedsky <jdlow2@gmail.com>
2019-01-06 20:44:07 -0500

    Installed express, made first api route

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('hello world!');
+});
+
+/*
+ *server.get('/api/users', (req, res) => {
+ *  let users;
+ *  db.find().then(r => users = r);
+ *  res.send(`helo, ${users}`);
+ *});
+ */
+
+server.listen(5500, () => console.log('hello from the server!'));
